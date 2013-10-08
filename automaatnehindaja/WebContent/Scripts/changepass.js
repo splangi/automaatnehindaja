a20e2c6a2f9444f430c35dd8eb876cddd68d1dff
johannesait <sl2mmer@ut.ee>
2013-10-08 11:26:34 +0300

    salasõna vahetus

---
+++ automaatnehindaja/WebContent/Scripts/changepass.js
@@ -0,0 +1,25 @@
+function checkPassword() {
+	
+	var oldPass = $( "[name='oldPass']" )[0].value;
+	var newPass1 = $( "[name='newPass1']" )[0].value;
+	var newPass2 = $( "[name='newPass2']" )[0].value;
+	
+	if (oldPass === "" || newPass1 === "" || newPass2 === "") {
+		$("#message").text("Täida kõik lahtrid!");
+	}
+	
+	else if (newPass1 !== newPass2) {
+		$("#message").text("Salasõnad ei kattu!");
+	}
+	else {
+		$.post( "/automaatnehindaja/ChangePassServlet", { oldPass : oldPass, newPass1: newPass1, newPass2: newPass2 }, 
+				function( data ) { 
+			if (data === "success") {
+				$("#message").text("Salasõna muudetud!").css("color", "green");
+			}
+			else if (data === "wrongPass") {
+				$("#message").text("Vale salasõna!");
+			}
+		});
+	}
+}
---
+++ automaatnehindaja/src/automaatnehindaja/ChangePassServlet.java
@@ -0,0 +1,92 @@
+package automaatnehindaja;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Servlet implementation class ChangePassServlet
+ */
+@WebServlet("/ChangePassServlet")
+public class ChangePassServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ChangePassServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String username = request.getRemoteUser();
+		
+		String oldPass = request.getParameter("oldPass");
+		String newPass1 = request.getParameter("newPass1");
+		String newPass2 = request.getParameter("newPass2");
+		
+		Connection c = null;
+		PreparedStatement stmt = null;
+		String statement;
+		ResultSet rs;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			c = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/automaatnehindaja", "root",
+					"t6urott");
+			
+			statement = "SELECT password FROM users WHERE username = ?;";
+			
+			stmt = c.prepareStatement(statement);
+			stmt.setString(1, username);
+			rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				 if (rs.getString(1).equals(oldPass)) {
+					 
+					statement = "UPDATE users SET password = ? WHERE username = ?;";
+					stmt = c.prepareStatement(statement);
+					stmt.setString(1, newPass1);
+					stmt.setString(2, username);
+					
+					stmt.executeUpdate();
+					response.getWriter().write("success");
+				 }
+				 else {
+					 response.getWriter().write("wrongPass");
+				 }
+			}
+
+		} catch (SQLException e) {
+			response.sendRedirect("/automaatnehindaja/error.html");
+		} catch (ClassNotFoundException f) {
+			response.sendRedirect("/automaatnehindaja/error.html");
+		}
+	}
+
+}
