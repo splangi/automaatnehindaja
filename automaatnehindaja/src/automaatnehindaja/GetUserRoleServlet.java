2b0fe5a2f4cd718269c6f31ea363306a2a751156
splangi <siimplangi@hotmail.com>
2013-10-24 21:43:05 +0300

    fixes #48, fixes #49

---
+++ automaatnehindaja/src/automaatnehindaja/GetUserRoleServlet.java
@@ -0,0 +1,35 @@
+package automaatnehindaja;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.istack.internal.logging.Logger;
+
+@WebServlet("/getRole")
+public class GetUserRoleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/plain");
+		
+		if (request.isUserInRole("admin")){
+			response.getWriter().write("admin");
+			System.out.println("admin");
+		}
+		else if (request.isUserInRole("tudeng")){
+			response.getWriter().write("tudeng");
+			System.out.println("tudeng");
+		}
+		else if (request.isUserInRole("responsible")){
+			response.getWriter().write("responsible");
+			System.out.println("responsible");
+		}
+	}
+
+}
