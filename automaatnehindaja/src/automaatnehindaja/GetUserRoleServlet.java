e9f13653a601526ac2350417b9fedc23b9f0fe01
johannesait <sl2mmer@ut.ee>
2013-10-25 18:37:27 +0300

    test

--- automaatnehindaja/src/automaatnehindaja/GetUserRoleServlet.java
+++ automaatnehindaja/src/automaatnehindaja/GetUserRoleServlet.java
@@ -8,8 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sun.istack.internal.logging.Logger;
-
 @WebServlet("/getRole")
 public class GetUserRoleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
